e5fbf908fe5a7108b151622815bf2b702b36698d
m-salamon <Moishe@Salamons-MBP.bitbean.com>
2017-07-28 11:36:42 -0400

    miner updates

--- repo/config.js
+++ repo/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-        host: process.env.DB_HOST, //'localhost',
-        user: process.env.DB_USER, //'user',
-        password: process.env.DB_PASSWORD,// 'password',
-        database: process.env.DB_NAME // 'simcha-organizer'
+        host: process.env.DB_HOST_DEVELOPMENT, //'localhost',
+        user: process.env.DB_USER_DEVELOPMENT, //'user',
+        password: process.env.DB_PASSWORD_DEVELOPMENT,// 'password',
+        database: process.env.DB_NAME_DEVELOPMENT // 'simcha-organizer'
     }
 });
 
